9fabd9d5a904a7efe860bb5fdd5368f3cb6d7424
Waleed Khalid <waleedkhalid99900@gmail.com>
2026-09-07 22:52:52 +0500

    ci: include dashboard dependencies and tensorflow-cpu in test workflow

--- tests/test_tensorflow_model.py
+++ tests/test_tensorflow_model.py
@@ -10,6 +10,8 @@ from pathlib import Path
 import numpy as np
 import pytest
 
+pytest.importorskip("tensorflow")
+
 from src.exceptions import ModelTrainingError
 from src.models.tensorflow_model import TensorFlowAQIModel
 
